409ef6e5162dac1da6e6011e77a955fc09bbeceb
Phạm Jin <vanquy33338888@gmail.com>
2021-05-12 23:47:26 +0700

    quý push

--- Back_End/quy_server/app.js
+++ Back_End/quy_server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
+require('dotenv').config();
 var app = express();
 // app.use(cors())
 // view engine setup
--- Back_End/quy_server/routes/index.js
+++ Back_End/quy_server/routes/index.js
@@ -3,17 +3,20 @@ var router = express.Router();
 var mysql = require('mysql');
 var session = require('express-session')
 var cors = require('cors')
-var multer  = require('multer')
-const nodemailer =  require('nodemailer');
+var multer = require('multer')
+// var upload = multer({ storage: storage })
+var nodemailer = require('nodemailer');
 var md5 = require('md5');
- 
-/* GET home page. */
+
+var jwt = require('jsonwebtoken');
+
 var con = mysql.createConnection({
   host: "localhost",
-  user     : 'root',
-  password : '',
-  database : 'dbcaycanh'
+  user: 'root',
+  password: '',
+  database: 'dbcaycanh'
 });
+con.connect();
 // var storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
 //     cb(null, './public/uploads')
@@ -24,169 +27,205 @@ var con = mysql.createConnection({
 //   }
 // })
 
+router.use(cors());
 
-// var upload = multer({ storage: storage })
-con.connect();
-router.get('/getdata', function(req, res, next) {
-  // Website you wish to allow to connect
+const getdata = (req, res, next) => {
+
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-    con.query("SELECT * FROM caycanh", function (err, result, fields) {
-      if (err) throw err;
-      res.send(result);
-    });
-});
+  con.query("SELECT * FROM caycanh", function (err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+  });
+}
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+router.get('/getdata',authenToken, getdata);
+
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 
-router.post('/add',cors(),  function(req, res, next) {
-   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-   // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-   // Request headers you wish to allow
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
- 
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.setHeader('Access-Control-Allow-Credentials', true);
-   let id = req.body.id;
-    let ten = req.body.name;
-    let img = req.body.img;
-    let Gia = req.body.Gia;
-    con.query("INSERT INTO caycanh VALUES ("+id+",'"+ ten+"', '"+img+"', "+Gia+")", (err, result)  => {
-      if (err)   res.send('Thêm Lỗi rồi', err);
-      res.send('Thêm Thành công');
-    });
-});
-
-router.post('/delete', cors(),  function(req, res, next) {
+router.post('/add', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  let id = req.body.id;
-  console.log(id);
-   con.query("DELETE FROM `caycanh` WHERE `caycanh`.`id` = "+id+"", (err, result)  => {
-     if (err)   res.send('Xóa Lỗi rồi', err);
-     res.send('Xóa dữ liệu Thành công');
-   });
-});
-router.put('/put', cors(),  function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
   let id = req.body.id;
   let ten = req.body.name;
-    let img = req.body.img;
-    let Gia = req.body.Gia;
-   con.query("UPDATE `caycanh` SET `name` = '"+ten +"', `img` = '"+img+"', `Gia` = '"+Gia+"' WHERE `caycanh`.`id` = "+id+"", (err, result)  => {
-     if (err)   res.send('Sửa Lỗi rồi', err);
-     res.send('Sửa dữ liệu Thành công');
-   });
+  let img = req.body.img;
+  let Gia = req.body.Gia;
+  con.query("INSERT INTO caycanh VALUES (" + id + ",'" + ten + "', '" + img + "', " + Gia + ")", (err, result) => {
+    if (err){
+      res.sendStatus(405);
+    }
+    else{
+      res.send({success :"Thêm dữ liệu thành công !"})
+    }
+    
+  });
+});
+
+router.delete('/xoa/:id', (req, res, next) => {
+  let id = req.params.id;
+  con.query("DELETE FROM `caycanh` WHERE `caycanh`.`id` = " + id + "", (err, result) => {
+    if (err){
+      res.sendStatus(405);
+    }
+    else{
+      res.send({success :"Xóa dữ liệu thành công !"})
+    }
+  });
+});
+
+router.put('/sua/:id', cors(), (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  let id = req.params.id;
+  let ten = req.body.name;
+  let img = req.body.img;
+  let Gia = req.body.Gia;
+
+  con.query("UPDATE `caycanh` SET `name` = '" + ten + "', `img` = '" + img + "', `Gia` = '" + Gia + "' WHERE `caycanh`.`id` = " + id + "", (err, result) => {
+    if (err){
+      res.send(err);
+    }
+    else{
+      res.send({success :"Sửa dữ liệu thành công !"})
+    }
+  });
+});
+
+router.get('/getdata/:page', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  let limit = 4;
+  let start = (req.params.page - 1) * limit;
+  con.query("SELECT * FROM caycanh limit " + start + ", " + limit + "", function (err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+router.get('/getpage', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  con.query("SELECT count(*) FROM caycanh ", function (err, result, fields) {
+    if (err) res.sendStatus(404);
+    res.send(Object.values(result[0]));
+  });
+});
+function authenToken (req, res, next) {
+  const authorizationHeader = req.headers['authorization'];
+  // 'Beaer [token]'
+  const token = authorizationHeader.split(' ')[1];
+  if (!token) res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    //console.log(err, data);
+    if (err) res.sendStatus(403);
+    next();
+  });
+}
+
+router.post('/login', (req, res, next) => {
+  let username = req.body.username;
+  let password = md5(req.body.password);
+  con.query("SELECT COUNT(*) as SoLuongUser FROM `user` WHERE gmail = '" + username + "' and matkhau = '" + password + "'", (err, result, fields) => {
+    if (err) {
+      res.sendStatus(405);
+    }
+    else {
+      let abc = Object.values(result[0]);
+      if (abc[0] == 1) {
+        const accessToken = jwt.sign({ username: username, password: password }, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '30s',
+        });
+        res.send({ accessToken: accessToken , Username : username});
+      }
+      else{
+        res.sendStatus(401);
+      }
+    }
+  });
 });
 
 
+router.get('/send', function (req, res, next) {
 
-
-
-router.get('/send', function(req, res, next) {
-   
-    res.send('heheh');
+  res.send('heheh');
 });
-router.get('/lay', function(req, res, next) {
-  
+router.get('/lay', function (req, res, next) {
+
   res.send(req.session.quy);
 
 });
-router.post('/send-mail', function(req, res) {
 
-  var transporter =  nodemailer.createTransport({ 
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-          user: 'vanquy33338888@gmail.com',
-          pass: '01652343938'
-      },
-      tls: {
-         
-          rejectUnauthorized: false
-      }
+router.post('/send-mail', (req, res) => {
+  var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'vanquy33338888@gmail.com',
+      pass: '01652343938'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
-  var mainOptions = { 
-      from: 'Phạm Jin',
-      to: req.body.mail,
-      subject: 'Test Thôi',
-      text: 'Đại học điẹn lực',
-    
+  var mainOptions = {
+    from: 'Phạm Jin',
+    to: req.body.mail,
+    subject: 'Test Thôi',
+    text: 'Đại học điẹn lực',
+
   }
-  transporter.sendMail(mainOptions, function(err, info){
-      if (err) {
-          console.log(err);
-          req.flash('mess', 'Lỗi gửi mail: '+err); 
-          res.redirect('/');
-      } else {
-          console.log('Message sent: ' +  info.response);
-          req.flash('mess', 'Một email đã được gửi đến tài khoản của bạn'); 
-          res.redirect('/');
-      }
+  transporter.sendMail(mainOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+      req.flash('mess', 'Lỗi gửi mail: ' + err);
+      res.redirect('/');
+    } else {
+      console.log('Message sent: ' + info.response);
+      req.flash('mess', 'Một email đã được gửi đến tài khoản của bạn');
+      res.redirect('/');
+    }
   });
 });
 
-router.post('/dangky',cors(),  function(req, res, next) {
-  // Website you wish to allow to connect
+router.post('/dangky', cors(), function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);  
-   let gmail = req.body.gmail;
-   let sdt = req.body.sdt;
-   let pass = md5(req.body.pass);
-   con.query("INSERT INTO `user` (`gmail`, `sdt`, `matkhau`) VALUES ('"+gmail+"', '"+ sdt+"', '"+ pass+"')", (err, result)  => {
-     if (err)   res.send('lỗi', err);
-     res.send(result);
-   });
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  let username = req.body.gmail;
+  let sdt = req.body.sdt;
+  let password = md5(req.body.pass);
+  con.query("INSERT INTO `user` (`gmail`, `sdt`, `matkhau`) VALUES ('" +username + "', '" + sdt + "', '" + password + "')", (err, result) => {
+    if (err){
+      res.sendStatus(405);
+    }
+    else{
+      res.send({success :"Đăng ký tài khoản thành công !"})
+    }
+  });
 });
+
+
 
 module.exports = router;
